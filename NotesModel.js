6a885b97a11edc8f0df4fbbf24e2d1f9fd21bc02
Bensarras <95305924+Bensarras@users.noreply.github.com>
2022-10-16 21:41:04 -0300

    Add files via upload

---
+++ NotesModel.js
@@ -0,0 +1,37 @@
+
+
+//TODO - Create Note Schema here having fields
+//      - noteTitle
+//      - noteDescription
+//      - priority (Value can be HIGH, LOW or MEDUIM)
+//      - dateAdded
+//      - dateUpdated
+
+
+
+const mongoose = require('mongoose');
+const noteSchema = new mongoose.Schema({
+    noteTitle: {
+        type: String,
+        required: true
+    },
+    noteDescription: {
+        type: String,
+        required: true
+    },
+    priority: {
+        type: String,
+        required: true,
+        values: ['HIGH', 'LOW', 'MEDIUM']
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now
+    },
+    dateUpdated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("note", noteSchema);
